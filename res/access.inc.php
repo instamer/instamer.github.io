fb3d5ca1394c6e3b323389b410cc611f25c98e59
instamer <bobesponjita@yandex.com>
2016-11-30 15:40:15 +0100

    Add files via upload

---
+++ res/access.inc.php
@@ -0,0 +1,30 @@
+<?php
+
+// Users data
+$imSettings['access']['webregistrations_gid'] = '23axc6a7';
+$imSettings['access']['users'] = array(
+	'admin' => array(
+		'groups' => array('8y9h8lg4'),
+		'id' => '8y9h8lg4',
+		'name' => 'Admin',
+		'password' => 'admon',
+		'email' => '',
+		'page' => 'index.html'
+	),
+	'nuevousuario' => array(
+		'groups' => array('m39i7yo4'),
+		'id' => 'm39i7yo4',
+		'name' => 'Nuevousuario',
+		'password' => 'zgc8bhe5',
+		'email' => '',
+		'page' => 'index.html'
+	)
+);
+
+// Admins list
+$imSettings['access']['admins'] = array('8y9h8lg4');
+
+// Page/Users permissions
+$imSettings['access']['pages'] = array();
+
+// End of file access.inc.php
